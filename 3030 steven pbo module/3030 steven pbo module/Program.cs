d0be35c863a23365a975c0f4dda9af9317eaeb07
SEdwin22 <133666486+SEdwin22@users.noreply.github.com>
2023-06-21 19:13:11 +0700

    Add files via upload

---
+++ 3030 steven pbo module/3030 steven pbo module/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+class Laptop
+{
+    public string Merk { get; set; }
+    public string Tipe { get; set; }
+
+    public void LaptopDinyalakan()
+    {
+        Console.WriteLine($"Laptop {Merk} {Tipe} menyala");
+    }
+
+    public void LaptopDimatikan()
+    {
+        Console.WriteLine($"Laptop {Merk} {Tipe} mati");
+    }
+
+    public virtual void BermainGame()
+    {
+        Console.WriteLine($"Laptop {Merk} {Tipe} sedang memainkan game");
+    }
+
+    public virtual void Ngoding()
+    {
+        Console.WriteLine("Ctak Ctak Ctak, error lagi!!");
+    }
+
+    internal void TampilkanSpesifikasi1()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+class Vivobook : Laptop
+{
+    public string Vga { get; set; }
+    public string Processor { get; set; }
+}
+
+class IdeaPad : Laptop
+{
+    public string Vga { get; set; }
+    public string Processor { get; set; }
+}
+
+class Predator : Laptop
+{
+    public string Vga { get; set; }
+    public string Processor { get; set; }
+
+    public override void BermainGame()
+    {
+        Console.WriteLine($"Laptop {Merk} {Tipe} sedang memainkan game di mode Predator");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Laptop laptop1 = new Vivobook
+        {
+            Merk = "ASUS",
+            Tipe = "Vivobook",
+            Vga = "Nvidia",
+            Processor = "Core i5"
+        };
+
+        Laptop laptop2 = new IdeaPad
+        {
+            Merk = "Lenovo",
+            Tipe = "IdeaPad",
+            Vga = "AMD",
+            Processor = "Ryzen"
+        };
+
+        Predator predator = new Predator
+        {
+            Merk = "Acer",
+            Tipe = "Predator",
+            Vga = "AMD",
+            Processor = "Core i7"
+        };
+
+        laptop1.LaptopDinyalakan();
+        laptop1.BermainGame();
+
+        laptop2.LaptopDinyalakan();
+        laptop2.Ngoding();
+
+        predator.LaptopDinyalakan();
+        predator.BermainGame();
+
+        laptop1.LaptopDimatikan();
+        laptop2.LaptopDimatikan();
+        predator.LaptopDimatikan();
+
+    }
+}
---
+++ 3030 steven pbo module/3030 steven pbo module/Programs.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class Laptopa1
+{
+    public string Merk1 { get; set; }
+    public string Tipe1 { get; set; }
+
+    public virtual void TampilkanSpesifikasi()
+    {
+        Console.WriteLine($"Spesifikasi Laptop:\nMerk: {Merk1}\nTipe: {Tipe1}");
+    }
+
+    internal void TampilkanSpesifikasi1()
+    {
+        throw new NotImplementedException();
+    }
+
+    internal void TampilkanSpek()
+    {
+        throw new NotImplementedException();
+    }
+
+    internal void TampilSpek()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+class Vivobook1 : Laptopa1
+{
+    public string Vga1 { get; set; }
+    public string Processor1 { get; set; }
+
+    public override void TampilSpek()
+    {
+        base.TampilSpek();
+        Console.WriteLine($"VGA: {Vga1}\nProcessor: {Processor1}");
+    }
+}
+class Program1
+{
+    private static object Laptopa1;
+
+    static void Main(string[] args) => _ = _ = Laptopa1.TampilSpek();
+}
